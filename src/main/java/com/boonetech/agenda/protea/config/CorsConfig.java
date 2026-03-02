f0ea0f6c51c4de72381e81b3481be9984164c015
jhonboone <jhonboone.dev@gmail.com>
2026-03-01 23:45:41 -0300

    fix: cors liberado para localhost

--- src/main/java/com/boonetech/agenda/protea/config/CorsConfig.java
+++ src/main/java/com/boonetech/agenda/protea/config/CorsConfig.java
@@ -14,8 +14,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // frontend local
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://localhost:5173"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
