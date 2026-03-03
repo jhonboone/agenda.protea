d6873c85be1b5476b03ce94057a4df4055bfa370
jhonboone <jhonboone.dev@gmail.com>
2026-03-02 21:16:58 -0300

    fix: cors

--- src/main/java/com/boonetech/agenda/protea/config/CorsConfig.java
+++ src/main/java/com/boonetech/agenda/protea/config/CorsConfig.java
@@ -14,11 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "https://localhost:5173",
-                                "https://agenda-protea.vercel.app"
-                        )
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
