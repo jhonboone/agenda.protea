f50f01b2d4182069545e1737c17006e81354a9fe
jhonboone <jhonboone.dev@gmail.com>
2026-03-02 22:24:17 -0300

    fix: RequestMapping

--- src/main/java/com/boonetech/agenda/protea/controller/AgendamentoController.java
+++ src/main/java/com/boonetech/agenda/protea/controller/AgendamentoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/v1/agendamentoss")
+@RequestMapping(value = "/api/v1/agendamento")
 @RequiredArgsConstructor
 public class AgendamentoController {
 
